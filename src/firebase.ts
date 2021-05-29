d0d21d706dbb6a32728630401f2acd9a9b88cdf9
Yash Chhatre <yashchhatre@Yashs-MacBook-Air.local>
2021-05-29 12:56:50 -0700

    setup firebase completely, able to write data successfully!

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,16 @@
-import * as firebase from "firebase";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import "firebase/auth"; 
+const firebaseConfig = {
+    apiKey: "AIzaSyDmza760gAOtqWDnVTArkNpbaPr3jfCjTk",
+    authDomain: "dormup-c85fb.firebaseapp.com",
+    projectId: "dormup-c85fb",
+    storageBucket: "dormup-c85fb.appspot.com",
+    messagingSenderId: "45859120733",
+    appId: "1:45859120733:web:bf07f7fe55d180add58fb4",
+    measurementId: "G-C2PMK2NDWL"
+  };
 
-let firebaseConfig = {
-  apiKey: "AIzaSyDmza760gAOtqWDnVTArkNpbaPr3jfCjTk",
-  authDomain: "dormup-c85fb.firebaseapp.com",
-  projectId: "dormup-c85fb",
-  storageBucket: "dormup-c85fb.appspot.com",
-  messagingSenderId: "45859120733",
-  appId: "1:45859120733:web:bf07f7fe55d180add58fb4",
-  measurementId: "G-C2PMK2NDWL",
-};
-
-const app = firebase.default.initializeApp(firebaseConfig);
-export const db = app.firestore(); 
-export const auth = app.auth(); 
+firebase.initializeApp(firebaseConfig); 
+export const db = firebase.firestore();
+export const auth = firebase.auth();
